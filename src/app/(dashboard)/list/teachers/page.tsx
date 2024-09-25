8087020a5318f0e00be221a199febbe2ec5c5a5f
Christopher Krebes <christopher.krebes@siv.de>
2024-09-25 15:28:27 +0200

    Finishing all homepages

---
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -0,0 +1,9 @@
+const TeacherListPage = () => {
+    return (
+        <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
+            TeacherListPage
+        </div>
+    );
+};
+
+export default TeacherListPage;
